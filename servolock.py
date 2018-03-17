01628412dd0e4d795535444485d0f4f9ff3af1e0
Rolf Ankli Junior <33335897+rolfjunior@users.noreply.github.com>
2018-03-17 15:22:23 -0300

    Add files via upload

--- servolock.py
+++ servolock.py
@@ -1,3 +1,4 @@
+#Reads Input from butons ans sensor ans writes comands to leds
 import RPi.GPIO as GPIO
 import threading
 import time
@@ -7,16 +8,22 @@ import cPickle as pickle
 
 pickle_filepath = "/home/pi/lockstate.pickle"
 
+print "servolock initiated"
+
+GPIO.setwarnings(False)
+
 class servolock:
 	def __init__(self):
 		GPIO.setmode(GPIO.BOARD)
 		self.time_stamp = time.time()
-		GPIO.setup(11, GPIO.IN)
-		GPIO.setup(13, GPIO.IN)
-		GPIO.setup(5, GPIO.IN)
-		GPIO.setup(10, GPIO.OUT)
-		GPIO.setup(12, GPIO.OUT)
-		GPIO.add_event_detect(11, GPIO.RISING, callback=self.lock)
+
+		GPIO.setup(7, GPIO.IN) #lock button
+		GPIO.setup(13, GPIO.IN) #unlock button
+		GPIO.setup(5, GPIO.IN) #sensor
+		GPIO.setup(3, GPIO.OUT) #red led
+		GPIO.setup(11, GPIO.OUT) #green led
+
+		GPIO.add_event_detect(7, GPIO.RISING, callback=self.lock)
 		GPIO.add_event_detect(13, GPIO.RISING, callback=self.unlock)
 		GPIO.add_event_detect(5, GPIO.BOTH, callback=self.sensor)
 				
@@ -41,32 +48,49 @@ class servolock:
 		os.system("python door_lock.py U")
 
 	def sensor (self, channel):
-		if (GPIO.input(5) != 1):
-			time.sleep(6)
-#			self.time_now = time.time()
-#			if(self.time_now - self.time_stamp) >= 5.1:
-			if (GPIO.input(5) != 1):
-				print "Door Closed"
-				doorstatus = "Closed"
-				lockstatus = "UnLocked"
-				with open(pickle_filepath, "wb") as pickle_handle:
-					pickle.dump(doorstatus, pickle_handle)
-					pickle.dump(lockstatus, pickle_handle)
-					pickle_handle.close()
-				print "The door is closed! Automatically Locking door\n"
-				os.system("python door_lock.py L")
-				GPIO.output(10,GPIO.HIGH)
-				GPIO.output(12,GPIO.LOW)
-#			self.time_stamp = self.time_now
-
-		else:
-			print "Door open"
+		GPIO.remove_event_detect(5)
+		print "Sensor change detected, deativated GPIO evente detect for sensor"
+		if (GPIO.input(5) == 1):
+			print "Door open detected"
 			doorstatus = "Open"
 			lockstatus = "UnLocked"
 			with open(pickle_filepath, "wb") as pickle_handle:
 				pickle.dump(doorstatus, pickle_handle)
 				pickle.dump(lockstatus, pickle_handle)
 				pickle_handle.close()
-			print "The door is open, making sure it has the correct status\n"			  
-			GPIO.output(10,GPIO.LOW)
-			GPIO.output(12,GPIO.HIGH)
+			GPIO.output(3,GPIO.LOW)
+			GPIO.output(11,GPIO.HIGH)
+		elif (GPIO.input(5) != 1):
+			print "Door closed detected"
+			doorstatus = "Closed"
+			lockstatus = "UnLocked"
+			print "Writing status to pickle"
+			print doorstatus
+			print lockstatus
+			with open(pickle_filepath, "wb") as pickle_handle:
+				pickle.dump(doorstatus, pickle_handle)
+				pickle.dump(lockstatus, pickle_handle)
+				pickle_handle.close()
+			print "Debounce"
+			time.sleep(5)
+			if (GPIO.input(5) != 1):
+				print "Door still closed, locking automatically"
+				os.system("python door_lock.py L")
+			else:
+				print "Door not closed anymore! Adjusting status and led"
+				doorstatus = "Open"
+				lockstatus = "UnLocked"
+				print "Writing status to pickle"
+				print doorstatus
+				print lockstatus
+				with open(pickle_filepath, "wb") as pickle_handle:
+					pickle.dump(doorstatus, pickle_handle)
+					pickle.dump(lockstatus, pickle_handle)
+					pickle_handle.close()
+				GPIO.output(3,GPIO.LOW)
+				GPIO.output(11,GPIO.HIGH)
+		else:
+			print "Door status unknown. Do nothing!\n"
+
+		print "end of sensor read command. Reactivating sensor pin read\n"
+		GPIO.add_event_detect(5, GPIO.BOTH, callback=self.sensor)
